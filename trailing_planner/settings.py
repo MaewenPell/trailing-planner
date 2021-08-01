ab581665f22f4bc40bf093b2f8bc2c9fd8b2462e
Maewen Pelletier <maewen.p@gmail.com>
2021-08-01 14:12:24 +0200

    add static

--- trailing_planner/settings.py
+++ trailing_planner/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -141,15 +141,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_ROOT = 'staticfiles'
+# # STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_ROOT_URL = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(STATIC_ROOT_URL, 'mainsite/static/')
+# STATIC_ROOT_URL = Path(__file__).resolve().parent.parent
+# STATIC_URL = os.path.join(STATIC_ROOT_URL, 'mainsite/static/')
+
+DEBUG = False
+STATIC_URL = "/static/"
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(STATIC_ROOT_URL, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
