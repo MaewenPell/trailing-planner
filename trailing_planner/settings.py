a7d51f71ef00c10bf9fe6c0b5cc369715185310d
Maewen Pelletier <maewen.p@gmail.com>
2021-08-01 14:19:38 +0200

    Edit settings

--- trailing_planner/settings.py
+++ trailing_planner/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # STATIC_ROOT_URL = Path(__file__).resolve().parent.parent
 # STATIC_URL = os.path.join(STATIC_ROOT_URL, 'mainsite/static/')
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 STATIC_URL = "/static/"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
